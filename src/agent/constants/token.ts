b6e44408de6c5f122b65be8b346a2de1933c8b02
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-03-12 02:38:33 +0530

    Added more tokens, Tokens available: usdc, usdt, wMON, wETH, wBTC, wSOL, gMON

--- src/agent/constants/token.ts
+++ src/agent/constants/token.ts
@@ -4,11 +4,17 @@ import { usdc_ABI } from "../abi/usdc-abi";
 import { wETH_ABI } from "../abi/wETH-abi";
 import { wBTC_ABI } from "../abi/wBTC-abi";
 import { wSOL_ABI } from "../abi/wSOL-abi";
+import { usdt_ABI } from "../abi/usdt-abi";
+import { gMON_ABI } from "../abi/gMON-abi";
 
 export const TOKENS = {
   usdc: {
     address: "0xf817257fed379853cDe0fa4F97AB987181B1E5Ea" as Address,
     abi: usdc_ABI,
+  },
+  usdt: {
+    address: "0x88b8E2161DEDC77EF4ab7585569D2415a1C1055D" as Address,
+    abi: usdt_ABI,
   },
   wMON: {
     address: "0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701" as Address,
@@ -25,5 +31,10 @@ export const TOKENS = {
   wSOL: {
     address: "0x5387C85A4965769f6B0Df430638a1388493486F1" as Address,
     abi: wSOL_ABI,
-  }
+  },
+  gMON: {
+    address: "0xaEef2f6B429Cb59C9B2D7bB2141ADa993E8571c3" as Address,
+    abi: gMON_ABI,
+  },
+  
 };
